7966aecf5051dde7c0ea9e42da2864420bb9e581
Alexander Smirnov <alexsmirnov0101@mail.ru>
2023-12-23 11:19:04 +0300

    fix processed image refresh

--- frontend/app.py
+++ frontend/app.py
@@ -10,6 +10,19 @@ from PIL import Image
 app = Flask(__name__)
 
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+
 @app.route('/', methods=['GET', 'POST'])
 def main():
     data = {'converted_img': None}
