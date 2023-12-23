513c8d425c97b67327bc7e18943d08e025d8f7a0
rw404 <s02190217@gse.cs.msu.ru>
2023-12-23 21:05:37 +0000

    coorect merge

--- frontend/app.py
+++ frontend/app.py
@@ -1,6 +1,7 @@
 import base64
 import io
 
+import cv2
 import numpy as np
 from flask import Flask, render_template, request
 from PIL import Image
@@ -9,13 +10,14 @@ from tritonclient.http import (InferenceServerClient, InferInput,
 
 from functools import lru_cache
 
+import time
+import os
 import cv2
 from gevent import monkey
 monkey.patch_all()
 
 app = Flask(__name__)
 app.config['UPLOADED_FILES'] = 'static/uploads'
-
 
 @lru_cache
 def get_client():
@@ -67,10 +69,9 @@ def main_back(img_list):
     answer.append(np.squeeze(results.as_numpy("output")))
     
     res = answer[0].clip(0, 255).astype(np.uint8)
-    # res = cv2.cvtColor(res, cv2.COLOR_BGR2RGB) # photo
     
     return res
-  
+
 @app.after_request
 def add_header(r):
     """
@@ -93,13 +94,16 @@ def main():
             img = Image.open(io.BytesIO(img_data)).convert('RGB')
             img.save('color_image.jpg')
             
+            
+            
         elif 'mask_img' in request.form:
             img_data = base64.b64decode(request.form['mask_img'].split(',')[1])
             img = Image.open(io.BytesIO(img_data)).convert('RGB')
             img.save('mask_image.jpg')
             
         elif 'convert_color_img' in request.form:
-            img = cv2.imread("image.jpg")
+            img = cv2.imread("color_image.jpg")
+        
             old_img = img.copy()
             old_img = cv2.cvtColor(old_img, cv2.COLOR_BGR2RGB)
             
@@ -115,7 +119,7 @@ def main():
             print(old_img.shape)
             res_img = np.hstack([old_img, res_img])
             res_img = cv2.cvtColor(res_img, cv2.COLOR_RGB2BGR)
-            cv2.imwrite("static/converted_img.jpg", res_img)
+            cv2.imwrite("static/converted_color_img.jpg", res_img)
             
         elif 'convert_mask_img' in request.form:
             img = cv2.imread("mask_image.jpg")
@@ -134,16 +138,35 @@ def main():
             res_img = np.hstack([old_img, res_img])
             res_img = cv2.cvtColor(res_img, cv2.COLOR_RGB2BGR)
             
+            print("HUI")
             cv2.imwrite("static/converted_mask_img.jpg", res_img)
         else:
+            imgs = []
             for i, file_storage in enumerate(request.files.getlist('files[]')):
-                Image.open(io.BytesIO(file_storage.read())).save(f'static/uploads/image_{i}.jpg')
+                img = Image.open(io.BytesIO(file_storage.read())).convert('RGB')
+                img.save(f'static/uploads/image_{i}.jpg')
+                img = np.array(img)
+                imgs.append(img)
+                
+            # convert imgs
+            gallery_img = create_img_gallery(imgs, imgs)
+            Image.fromarray(gallery_img).save('static/gallery_img.jpg')
 
     return render_template('index.html')
 
 
 def create_img_gallery(old_imgs, new_imgs):
-    pass
+    width = old_imgs[0].shape[1]
+    res_img = []
+    for old_img, new_img in zip(old_imgs, new_imgs):
+        h, w = old_img.shape[:2]
+        ratio = width / w
+        new_h = int(ratio * h)
+        new_w = int(ratio * w) 
+        old_img = cv2.resize(old_img, (new_w, new_h))
+        new_img = cv2.resize(new_img, (new_w, new_h))
+        res_img.append(np.hstack([old_img, new_img]))
+    return np.vstack(res_img)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
